3148b9671d9945089d953c0c304b6cec1e71ccc1
Yuichiro Hanada <hanada@kuis.kyoto-u.ac.jp>
2013-11-13 15:31:41 +0900

    Remove 40-yatex.el and add 24-tex.el.

---
+++ .emacs.d/inits/24-tex.el
@@ -0,0 +1,6 @@
+;==================================================
+; latex-mode
+;==================================================
+(add-to-list 'auto-mode-alist '("\\.tex" . latex-mode))
+(add-to-list 'auto-mode-alist '("\\.bib" . bibtex-mode))
+
--- .emacs.d/inits/40-yatex.el
+++
@@ -1,7 +0,0 @@
-;==================================================
-; YaTeX
-;==================================================
-(setq auto-mode-alist
-      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-(setq YaTeX-kanji-code 3)
