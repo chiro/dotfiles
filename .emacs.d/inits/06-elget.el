e207a568a7cefce60b4a549f9020d610f2be9c20
Yuichiro Hanada <peanut2626@gmail.com>
2014-08-05 02:16:44 +0900

    Add an el-get recipe for ddskk.

--- .emacs.d/inits/06-elget.el
+++ .emacs.d/inits/06-elget.el
@@ -10,6 +10,20 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
+(setq el-get-sources
+      '((:name ddskk-15.1
+               :description "A Japanese input method on Emacs."
+               :website "http://openlab.ring.gr.jp/skk/ddskk.html"
+               :type http-tar
+               :options ("xzf")
+               :autoloads nil
+               :url "http://openlab.ring.gr.jp/skk/maintrunk/ddskk-15.1.tar.gz"
+               :info "doc/skk.info"
+               :features ("skk-setup")
+               :build `((,el-get-emacs "-batch" "-q" "-no-site-file" "-l" "SKK-MK" "-f" "SKK-MK-compile")
+                        (,el-get-emacs "-batch" "-q" "-no-site-file" "-l" "SKK-MK" "-f" "SKK-MK-compile-info")
+                        ("mv" "skk-setup.el.in" "skk-setup.el")))))
+
 ;; Install packages by el-get
 (el-get 'sync '(howm))
-(el-get 'sync '(ddskk))
+(el-get 'sync '(ddskk-15.1))
