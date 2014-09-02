29c4fca4f76523b228895d0ab9d3dd4c4277292d
chiro <peanut2626@gmail.com>
2014-09-02 23:38:27 +0900

    Add settings of ghc-mod.

--- .emacs.d/inits/22-haskell.el
+++ .emacs.d/inits/22-haskell.el
@@ -3,4 +3,15 @@
 ;==================================================
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(custom-set-variables
+ '(haskell-tags-on-save t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t))
 
+; ghc-mod
+(setenv "PATH" (concat "~/.cabal/bin;" (getenv "PATH")))
+(add-to-list 'exec-path "~/.cabal/bin")
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
