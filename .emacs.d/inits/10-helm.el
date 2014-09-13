5535781cd521510d63711c8a8c17f5b181ad088d
chiro <peanut2626@gmail.com>
2014-09-13 11:34:11 +0900

    Use helm-ghq.

--- .emacs.d/inits/10-helm.el
+++ .emacs.d/inits/10-helm.el
@@ -5,9 +5,25 @@
 (helm-descbinds-mode)
 
 (define-key global-map (kbd "M-x") 'helm-M-x)
-(define-key global-map (kbd "C-x f") 'helm-find-files)
-(define-key global-map (kbd "C-x b") 'helm-for-files)
+(define-key global-map (kbd "C-x C-f") 'helm-for-files)
 
 ;; hack for C-h
 (eval-after-load 'helm
   '(define-key helm-map (kbd "C-h") 'delete-backward-char))
+
+;; helm-ghq
+(require 'helm-ghq)
+
+(require 'helm-files)
+(defadvice helm-for-files
+    (around helm-for-files-no-highlight activate)
+  (let ((helm-mp-highlight-delay nil))
+    ad-do-it))
+
+(defconst helm-for-files-preferred-list
+  '(helm-source-buffers-list
+    helm-source-recentf
+    helm-source-file-cache
+    helm-source-ghq
+    helm-source-files-in-current-dir
+    ))
