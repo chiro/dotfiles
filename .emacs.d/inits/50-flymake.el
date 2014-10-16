90e3c507c01e2eac08dbd45aa8639e3603b0a0fc
Yuichiro Hanada <hanada@kuis.kyoto-u.ac.jp>
2014-10-16 17:11:33 +0900

    Quit using flymake.

--- .emacs.d/inits/50-flymake.el
+++
@@ -1,6 +0,0 @@
-;==================================================
-; flymake
-;==================================================
-(require 'flymake)
-(custom-set-faces '(flymake-errline ((((class color)) (:background "Pink" :underline "Red"))))
-                  '(flymake-warnline ((((class color)) (:background "LighBlue" :underline "Yellow")))))
