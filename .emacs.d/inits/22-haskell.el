5878bae3458d3e0ad4f166f805191e64395b355b
chiro <peanut2626@gmail.com>
2015-02-03 19:08:51 +0900

    Tweak the config of haskell-mode.

--- .emacs.d/inits/22-haskell.el
+++ .emacs.d/inits/22-haskell.el
@@ -1,17 +1,32 @@
 ;==================================================
 ; haskell mode
 ;==================================================
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 (custom-set-variables
  '(haskell-tags-on-save t)
  '(haskell-process-suggest-remove-import-lines t)
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t))
 
+(with-eval-after-load 'haskell-mode
+  (require 'ghc)
+
+  (add-hook 'haskell-mode-hook 'my/haskell-mode-hook)
+  (setq ghc-module-command (executable-find "ghc-mod")))
+
 ; ghc-mod
 (setenv "PATH" (concat "~/.cabal/bin;" (getenv "PATH")))
 (add-to-list 'exec-path "~/.cabal/bin")
-(autoload 'ghc-init "ghc" nil t)
-(autoload 'ghc-debug "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
+(defun my/haskell-mode-hook ()
+  (turn-on-haskell-doc-mode)
+  (turn-on-haskell-indentation)
+  (setq flycheck-checker 'haskell-ghc)
+
+  (ghc-abbrev-init)
+  (ghc-type-init)
+  (unless ghc-initialized
+    (ghc-comp-init)
+    (setq ghc-initialized t))
+
+  ;; for auto-complete
+  (add-to-list 'ac-sources 'ac-source-ghc-mod))
