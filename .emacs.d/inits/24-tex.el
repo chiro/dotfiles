167aaee3074ddc8b46d369621cbe4d97649a0b17
chiro <peanut2626@gmail.com>
2015-02-03 18:47:59 +0900

    Add configs for synctex.

--- .emacs.d/inits/24-tex.el
+++ .emacs.d/inits/24-tex.el
@@ -1,6 +1,31 @@
 ;==================================================
 ; latex-mode
 ;==================================================
+;;; Code:
 (add-to-list 'auto-mode-alist '("\\.tex" . latex-mode))
 (add-to-list 'auto-mode-alist '("\\.bib" . bibtex-mode))
 
+(setq tex-default-mode 'latex-mode)
+
+(defun tex-view ()
+  (interactive)
+  (tex-send-command "/usr/bin/open -a Skim.app" (tex-append tex-print-file ".pdf")))
+
+(defun skim-forward-search ()
+  (interactive)
+  (let* ((ctf (buffer-name))
+         (mtf (tex-main-file))
+         (pf (concat (car (split-string mtf "\\.")) ".pdf"))
+         (ln (format "%d" (line-number-at-pos)))
+         (cmd "~/Applications/Skim.app/Contents/SharedSupport/displayline")
+         (args (concat ln " " pf " " ctf)))
+    (message (concat cmd " " args))
+    (process-kill-without-query
+     (start-process-shell-command "displayline" nil cmd args))))
+
+(add-hook 'latex-mode-hook
+          '(lambda ()
+             (define-key latex-mode-map (kbd "C-c s") 'skim-forward-search)))
+
+(provide '24-tex)
+;;; 24-tex.el ends here
