4368ba1bfcd8ddbb75e82d0521b5797ee3696ec9
chiro <i@chir.jp>
2017-07-10 01:01:57 +0900

    Update google-c-style configuration.

--- .emacs.d/inits/21-cpp.el
+++ .emacs.d/inits/21-cpp.el
@@ -4,14 +4,16 @@
 (use-package google-c-style
   :init
   (add-hook 'c-mode-common-hook 'google-set-c-style)
-  (add-hook 'c-mode-common-hook 'google-make-newline-indent))
+  (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+  :ensure t
+  :defer 1
+  :config
+  (setq c-hungry-delete-key t)
 
-(setq c-hungry-delete-key t)
-
-(setq auto-mode-alist
-      (append
-       '(("\\.c$"   . c-mode))
-       '(("\\.h$"   . c++-mode))
-       '(("\\.hpp$" . c++-mode))
-       '(("\\.cpp$" . c++-mode))
-       auto-mode-alist))
+  (setq auto-mode-alist
+        (append
+         '(("\\.c$"   . c-mode))
+         '(("\\.h$"   . c++-mode))
+         '(("\\.hpp$" . c++-mode))
+         '(("\\.cpp$" . c++-mode))
+         auto-mode-alist)))
