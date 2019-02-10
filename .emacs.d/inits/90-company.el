d47b0a8f69d1e9cec3b32f2fdfc99dec3eed66d9
chiro <i@chir.jp>
2019-02-10 10:30:46 +0900

    Fix indentation in 90-company.el.

--- .emacs.d/inits/90-company.el
+++ .emacs.d/inits/90-company.el
@@ -1,8 +1,7 @@
 (use-package company
-  :defer 2
   :config
   (use-package company-ghc
     :config (add-to-list 'company-backends 'company-ghc))
   (use-package company-go
-     :config (add-to-list 'company-backends 'company-go))
+    :config (add-to-list 'company-backends 'company-go))
   (global-company-mode))
