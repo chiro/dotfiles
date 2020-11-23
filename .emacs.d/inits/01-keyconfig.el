cc506248ee31d878fc46b5c75e9550465dbc2ad7
chiro <i@chir.jp>
2020-11-23 22:22:09 +0900

    Assign C-@ to set-mark-command explicitly.

--- .emacs.d/inits/01-keyconfig.el
+++ .emacs.d/inits/01-keyconfig.el
@@ -4,4 +4,7 @@
 ;; set C-h to BackSpace
 (global-set-key "\C-h" 'backward-delete-char)
 
+;; Set C-@ to set-mark-command
+(global-set-key "\C-@" 'set-mark-command)
+
 (defalias 'yes-or-no-p 'y-or-n-p)
