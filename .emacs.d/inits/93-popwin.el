50117dce00ee7fb7f6d954b71be1f9b7e2ab9099
chiro <i@chir.jp>
2020-11-24 00:03:05 +0900

    Remove helm-related config from popwin.el.

--- .emacs.d/inits/93-popwin.el
+++ .emacs.d/inits/93-popwin.el
@@ -2,7 +2,4 @@
   :defer 1
   :config
   (popwin-mode 1)
-
-  (setq helm-samewindow nil)
-  (push '("helm" :regexp t :height 0.4) popwin:special-display-config)
   (push '(compilation-mode :noselect t) popwin:special-display-config))
