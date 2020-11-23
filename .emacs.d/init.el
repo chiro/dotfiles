f64d71c4a13a0a0b145ad6c2f0671ee2e2342a0f
chiro <i@chir.jp>
2020-11-23 23:05:35 +0900

    Clean up custom-set-variables.
    It's per-host settings. It should not be in the repository.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,10 @@
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
 (when (require 'package nil t)
-  (add-to-list 'package-archives '("stable-melpa" . "https://stable.melpa.org/packages/") t)
+  (add-to-list 'package-archives
+               '("stable-melpa" . "https://stable.melpa.org/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize)
   ;; Install use-package if not there
   (unless (require 'use-package nil t)
@@ -19,16 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(anzu-deactive-region t)
- '(anzu-mode-lighter "")
- '(anzu-search-threshold 1000)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-tags-on-save t)
- '(package-selected-packages
-   (quote
-    (scala-mode haml-mode markdown-mode yaml-mode json-mode git-commit-mode projectile haskell-mode go-mode ddskk rust-mode anzu racer use-package company-go company org solarized-theme))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
