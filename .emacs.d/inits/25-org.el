c6a5c1d259dce725a9131c1a0b356b18a4968b8d
chiro <i@chir.jp>
2020-12-13 00:20:22 +0900

    Update org-capture-template.

--- .emacs.d/inits/25-org.el
+++ .emacs.d/inits/25-org.el
@@ -1,17 +1,21 @@
 ;==================================================
 ; Org mode
 ;==================================================
-(use-package org-install
+(use-package org
+  :ensure t
   :mode (("\\.org$" . org-mode))
   :config
   (setq org-startup-truncated nil)
   (setq org-return-follows-link t)
-  (setq org-directory "~/memo/")
-  (setq org-default-notes-file (concat org-directory "agenda.org"))
-  (setq org-remember-templates
-        '(("Todo" ?t "** TODO %?\n %i\n %a\n %t" nil "Inbox")
-          ("Bug"  ?b "** TODO $? :bug:\n %i\n %a\n %t" nil "Inbox")
-          ("Idea" ?i "** %?\n %i\n %a\n %t" nil "New Ideas")
-          ("Wait" ?w "** TODO %? :wait:\n %i\n %a\n %t" nil "Wait")
-          ))
-  )
+  (setq org-directory "~/org/")
+  (setq org-default-notes-file "notes.org"))
+
+(use-package org-capture
+  :config
+  (setq org-capture-templates
+        '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
+           "* TODO %?\n  %i\n  %a")
+          ("n" "note" entry (file+headline "~/org/notes.org" "Notes")
+           "* %?\n %i\n %a\n %T")))
+  :init
+  (bind-key "C-c c" 'org-capture))
