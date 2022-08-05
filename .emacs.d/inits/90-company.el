84b4f9a7ebe56b51288bbd10bbf8cabb971138cd
chiro <i@chir.jp>
2022-08-05 15:19:16 +0900

    Remove company-ghc.

--- .emacs.d/inits/90-company.el
+++ .emacs.d/inits/90-company.el
@@ -1,7 +1,5 @@
 (use-package company
   :config
-  (use-package company-ghc
-    :config (add-to-list 'company-backends 'company-ghc))
   (use-package company-go
     :config (add-to-list 'company-backends 'company-go))
   (global-company-mode))
