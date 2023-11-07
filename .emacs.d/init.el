bc9c48fccc8211dbbc80d79bf6446503e1a11399
chiro <i@chir.jp>
2023-11-08 00:17:23 +0900

    .emacs: Separate custom variables into a new file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,15 +21,10 @@
   :ensure t
   :config (init-loader-load (concat user-emacs-directory "inits")))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package zenburn-theme
+  :ensure t
+  :config (load-theme 'zenburn t))
+
+;; Put M-x customize stuff in a separate file
+(setq custom-file (concat user-emacs-directory "custom-file.el"))
+(load custom-file 'noerror)
