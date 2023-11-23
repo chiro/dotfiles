a76ba7b90b61964928f176afa3a917592a767e16
chiro <i@chir.jp>
2023-11-23 22:18:54 +0900

    .emacs: Remove ruby-mode settings
    It's pre-installed. There is no need to install it
    from elpa/melpa.

--- .emacs.d/inits/26-ruby.el
+++ .emacs.d/inits/26-ruby.el
@@ -1,12 +1,2 @@
-(use-package ruby-mode
-  :mode (("\\.rb$" . ruby-mode)
-         ("Gemfile$" . ruby-mode))
-  :init
-  (add-hook 'ruby-mode-hook
-            '(lambda ()
-               (setq tab-width 2)
-               (setq ruby-indent-level tab-width)
-               (setq ruby-deep-indent-paren-style nil))))
-
 (use-package haml-mode
   :mode (("\\.haml$" . haml-mode)))
