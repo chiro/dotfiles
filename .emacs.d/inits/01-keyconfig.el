b110f32127caaf614094279b40030bb5edee7425
chiro <i@chir.jp>
2024-01-03 23:11:47 +0900

    emacs: Use kbd function

--- .emacs.d/inits/01-keyconfig.el
+++ .emacs.d/inits/01-keyconfig.el
@@ -1,10 +1,10 @@
 ;; UNDO
-(global-set-key "\C-z" 'undo)
+(global-set-key (kbd "C-z") 'undo)
 
 ;; set C-h to BackSpace
-(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key (kbd "C-h") 'backward-delete-char)
 
 ;; Set C-@ to set-mark-command
-(global-set-key "\C-@" 'set-mark-command)
+(global-set-key (kbd "C-@") 'set-mark-command)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
