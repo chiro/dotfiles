aa3e5b02081c50a21b2ed201c85685504dec871b
chiro <i@chir.jp>
2024-01-09 23:37:53 +0900

    emacs: Use python-docstring and pip-requirements

--- .emacs.d/inits/29-python.el
+++ .emacs.d/inits/29-python.el
@@ -6,3 +6,8 @@
                (setq indent-level 4)
                (setq python-indent 4)))
   :mode (("\\.py$" . python-mode)))
+
+(use-package python-docstring
+  :hook python-mode)
+
+(use-package pip-requirements :defer t)
