4b3818da471c5994ec6e8fee00fbd420d8248732
chiro <i@chir.jp>
2024-01-09 23:36:26 +0900

    emacs: Clean up ivy configuration

--- .emacs.d/inits/10-ivy.el
+++ .emacs.d/inits/10-ivy.el
@@ -1,24 +1,31 @@
 (use-package counsel
-  :ensure t
-  :defer 2
   :config
-  (ivy-mode 1)
   (counsel-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-height 30)
   :bind (("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
-         ("C-x b" . ivy-switch-buffer)
          ("C-c g" . counsel-git)))
 
+(use-package ivy
+  :after (counsel)
+  :diminish ivy-mode
+  :bind (("C-x b" . ivy-switch-buffer)
+         ("C-c C-r" . ivy-resume))
+  :config
+  (ivy-mode 1)
+
+  ;; Add recent files to `ivy-switch-buffer`
+  (setq ivy-use-virtual-buffers t)
+
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-height 10)
+  (setq ivy-virtual-abbreviate nil))
+
 (use-package swiper
-  :ensure t
   :config
   (defvar swiper-include-line-number-in-search t)
-  :bind (("C-s" . swiper)))
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper--isearch-backward)))
 
 (use-package ivy-rich
-  :ensure t
   :config
   (ivy-rich-mode 1))
