1e36b32728cd04da45ba07c6d192e695a4e4a94a
chiro <i@chir.jp>
2024-10-13 01:54:57 +0900

    .emacs: Add more key binds for set-mark-command

--- .emacs.d/inits/01-keyconfig.el
+++ .emacs.d/inits/01-keyconfig.el
@@ -3,5 +3,7 @@
 
 ;; Set C-@ to set-mark-command
 (global-set-key (kbd "C-@") 'set-mark-command)
+(global-set-key (kbd "C-!") 'set-mark-command)
+(global-set-key (kbd "C-#") 'set-mark-command)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
