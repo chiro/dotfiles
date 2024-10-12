98cfbae84f77bdadb3d20d93db8a237d7b33f432
chiro <i@chir.jp>
2024-10-12 15:29:28 +0900

    .emacs.d: Update haskell.el

--- .emacs.d/inits/22-haskell.el
+++ .emacs.d/inits/22-haskell.el
@@ -1,23 +1,11 @@
 ;==================================================
 ; haskell mode
 ;==================================================
-(custom-set-variables
- '(haskell-tags-on-save t)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t))
-
 (use-package haskell-mode
   :ensure t
   :mode (("\\.hs$" . haskell-mode))
   :config
-  (use-package ghc)
-  (add-hook 'haskell-mode-hook 'my/haskell-mode-hook)
-  (setq ghc-module-command (executable-find "ghc-mod")))
-
-; ghc-mod
-(setenv "PATH" (concat "~/.cabal/bin;" (getenv "PATH")))
-(add-to-list 'exec-path "~/.cabal/bin")
+  (add-hook 'haskell-mode-hook 'my/haskell-mode-hook))
 
 (defun my/haskell-mode-hook ()
   (turn-on-haskell-doc-mode)
