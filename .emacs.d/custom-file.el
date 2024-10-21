eaf688298219303d21a8f1f39d5b30befadf6795
chiro <i@chir.jp>
2024-10-21 16:31:57 +0900

    Use use-packages :custom

--- .emacs.d/custom-file.el
+++ .emacs.d/custom-file.el
@@ -3,9 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(anzu-deactive-region t)
- '(anzu-mode-lighter "")
- '(anzu-search-threshold 1000)
  '(package-selected-packages nil))
 
 (custom-set-faces
--- .emacs.d/inits/06-anzu.el
+++ .emacs.d/inits/06-anzu.el
@@ -2,7 +2,7 @@
   :defer 1
   :config
   (global-anzu-mode +1)
-  (custom-set-variables
-   '(anzu-mode-lighter "")
-   '(anzu-deactive-region t)
-   '(anzu-search-threshold 1000)))
+  :custom
+  (anzu-mode-lighter "")
+  (anzu-deactive-region t)
+  (anzu-search-threshold 1000))
