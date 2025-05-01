24fc8be9cb5f08e03a5d59ebdc3cd8618f168b26
chiro <i@chir.jp>
2025-05-02 00:18:50 +0900

    .emacs: Clean up 10-ivy.el

--- .emacs.d/inits/10-ivy.el
+++ .emacs.d/inits/10-ivy.el
@@ -1,32 +1,31 @@
 (use-package counsel
-  :config
-  (counsel-mode 1)
   :diminish counsel-mode
   :bind (("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
-         ("C-c g" . counsel-git)))
+         ("C-c g" . counsel-git))
+  :config
+  (counsel-mode 1))
 
 (use-package ivy
-  :after (counsel)
+  :after counsel ; Ensure counsel is loaded before ivy tries to use it implicitly or explicitly
   :diminish ivy-mode
   :bind (("C-x b" . ivy-switch-buffer)
          ("C-c C-r" . ivy-resume))
-  :config
-  (ivy-mode 1)
-
+  :custom
   ;; Add recent files to `ivy-switch-buffer`
-  (setq ivy-use-virtual-buffers t)
-
-  (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-height 10)
-  (setq ivy-virtual-abbreviate nil))
+  (ivy-use-virtual-buffers t)
+  (ivy-count-format "(%d/%d) ")
+  (ivy-height 10)
+  (ivy-virtual-abbreviate nil)
+  :config
+  (ivy-mode 1))
 
 (use-package swiper
-  :config
-  (defvar swiper-include-line-number-in-search t)
+  :after ivy ; Explicitly state dependency
   :bind (("C-s" . swiper)
-         ("C-r" . swiper--isearch-backward)))
+         ("C-r" . swiper)))
 
 (use-package ivy-rich
+  :after ivy
   :config
   (ivy-rich-mode 1))
